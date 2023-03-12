ab5471216b22bc8b26ed0f4a8e063838214886d0
Anderson Martins <andersonmcadastros@gmail.com>
2023-03-12 19:30:18 +0000

    Seed SQL.txt

--- SalesWebMvc/Models/Seller.cs
+++ SalesWebMvc/Models/Seller.cs
@@ -32,15 +32,6 @@ namespace SalesWebMvc.Models
             BaseSalary = baseSalary;
             Department = department;
         }
-        public Seller(int id, string name, string email, DateTime birthDate, double baseSalary, Department department)
-        {
-            Id = id;
-            Name = name;
-            Email = email;
-            BirthDate = birthDate;
-            BaseSalary = baseSalary;
-            Department = department;
-        }
 
         public void AddSales(SalesRecord sr)
         {
